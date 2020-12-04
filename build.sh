b6fd41495a2065d5d064b0714565341599f6f43c
higuchi.fumito <higuchi.fumito@mail.canon>
2020-12-05 00:22:36 +0900

    fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-npx react-native bundle --platform android --entry-file example/index.js --bundle-output example/android/app/src/main/assets/index.android.bundle --assets-dest example/android/app/src/main/res/ $@ \
+rm -rf example/android/app/build \
+&& npx react-native bundle --platform android --entry-file example/index.js --bundle-output example/android/app/src/main/assets/index.android.bundle --assets-dest example/android/app/src/main/res/ \
 && npx jetify \
 && cd example/android \
-&& ./gradlew assembleDebug
-
+&& gradle assembleDebug
